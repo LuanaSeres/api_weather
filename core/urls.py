c68a3607b43634ffa10def9e98d53df09a608a1f
Luana Seres Girotto <123522626+LuanaSeres@users.noreply.github.com>
2024-03-08 13:38:25 -0300

    feat(mongo): connect with mongo
    Connect mongodb and create an repository to interact with database

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from weather.views import WeatherView
+from weather.views import WeatherView, WeatherGenerate
 
 urlpatterns = [
-    path('', WeatherView.as_view()),
+    path('', WeatherView.as_view(), name='Weather View'),
+    path('', WeatherGenerate.as_view(), name='Weather Generate'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,21 +1,28 @@
-from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
-from .models import WeatherEntity
-
 from datetime import datetime
 from random import randrange
+from django.views import View
+from django.shortcuts import render, redirect
+from .models import WeatherEntity
+from .repositories import WeatherRepository
 
 class WeatherView(View):
     def get(self, request):
-        weathers = []
-        for i in range(10):
-            weathers.append(
-                WeatherEntity(
-                    temperature=randrange(start=17, stop=40),
-                    date=datetime.now()
-                )
-            )
-        #return HttpResponse(weathers)
-        
-        return render(request, "home.html", {"weathers" : weathers})
+        repository = WeatherRepository(collectionName='weathers')
+        weathers = repository.getAll()
+        return render(request, "home.html", {"weathers":weathers})
+    
+
+class WeatherGenerate(View):
+    def get(self, request):
+        repository = WeatherRepository(collectionName='weathers')
+        # wheater = WeatherEntity(
+        #     temperature=randrange(start=17, stop=40),
+        #     date=datetime.now()
+        # )
+        wheater = {
+            "temperature" : 28,
+            "date": "hoje"
+            }
+        repository.insert(wheater)
+
+        return redirect('Weather View')

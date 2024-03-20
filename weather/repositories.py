5d456ba1f34abaebba73929b731ba9addf46469f
Luana Seres Girotto <123522626+LuanaSeres@users.noreply.github.com>
2024-03-20 15:32:43 -0300

    feat(serializer): added serializer
    serializer added and created forms

--- weather/repositories.py
+++ weather/repositories.py
@@ -2,14 +2,13 @@ from django.conf import settings
 import pymongo
 
 class WeatherRepository:
-
     collection = ''
 
     def __init__(self, collectionName) -> None:
         self.collection = collectionName
 
     def getConnection(self):
-        client = pymongo.MongoClient(getattr(settings,"MONGO_CONNECTION_STRING"))
+        client = pymongo.MongoClient(getattr(settings, "MONGO_CONNECTION_STRING"))
         connection = client[getattr(settings, "MONGO_DATABASE_NAME")]
         return connection
     
@@ -18,9 +17,23 @@ class WeatherRepository:
         collection = conn[self.collection]
         return collection
     
-    def getAll(self):
-        document = self.getCollection().find({})
+    def getById(self, id):
+        document = self.getCollection().find_one({"_id": id})
         return document
+    
+    def getAll(self):
+        documents = self.getCollection().find({})
+        return documents
+    
+    def getByAttribute(self, attribute, value):
+        documents = self.getCollection().find({attribute: value})
+        return documents
     
     def insert(self, document):
         self.getCollection().insert_one(document)
+
+    def delete(self, id):
+        self.getCollection().delete_one({"_id": id})
+
+    def deleteAll(self):
+        self.getCollection().delete_many({})

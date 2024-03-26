6a51cd2f9d263a6794268d506d4d3ac994f32877
Luana Seres Girotto <123522626+LuanaSeres@users.noreply.github.com>
2024-03-26 18:23:59 -0300

    refactor: some small changes to the code

--- weather/models.py
+++ weather/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class WeatherEntity:
 
-  def __init__(self, temperature, date, city='', atmosphericPressure='', humidity='', weather='') -> None:
+  def __init__(self, temperature, date, city='', atmosphericPressure=0, humidity=0, weather='') -> None:
     self.temperature = temperature 
     self.city = city
     self.atmosphericPressure = atmosphericPressure
@@ -13,6 +13,11 @@ class WeatherEntity:
   def __str__(self) -> str:
     return (f"Weather <{self.temperature}>")
   
+  def __getattribute__ (self, __name: str) -> any:
+    if (__name == 'date'):
+      return object.__getattribute__(self, __name).strftime("%d%m%Y %H:%M:%S")
+    else:
+      return object.__getattribute__(self, __name)
   
   
   
